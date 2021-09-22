f19ed4ba2e42ee6ed631a554f0f2d9a34ac049b1
Md Sarwar Zahan <Sarwar.Zahan@bmw.de>
2021-09-22 14:59:43 +0200

    Add endpoints

--- src/main/java/com/alpscron/boundary/ProductResource.java
+++ src/main/java/com/alpscron/boundary/ProductResource.java
@@ -1,4 +1,47 @@
 package com.alpscron.boundary;
 
+import com.alpscron.entity.Product;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/product")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
 public class ProductResource {
+
+   public static List<Product> products = new ArrayList<>();
+
+    @GET
+    public List<Product> getAllProduct(){
+        return products;
+
+    }
+
+    @POST
+    public Response addProduct(Product product){
+        products.add(product);
+        return Response.ok(product).build();
+    }
+
+    @PUT
+    @Path("{id}/{brand}")
+    public Response updateProduct(@PathParam("id") int id, @PathParam("brand") String brand){
+        products = products.stream().map(product->{
+            if(product.getProductId() == id){
+                product.setProductBrand(brand);
+            }
+            return product;
+        }).collect(Collectors.toList());
+        return Response.ok(products).build();
+    }
+
+
+
+
 }
